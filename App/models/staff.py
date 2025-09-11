226828a3857fce9457391e0c6c6b82342595898e
SciPhi-of-n <jonathan.bruce@my.uwi.edu>
2025-09-11 08:42:08 -0800

    Created staff.py
    Initial creation of staff model

---
+++ App/models/staff.py
@@ -0,0 +1,13 @@
+from .user import User
+from App.database import db
+
+class Student(User):
+  __tablename__ = 'student'
+  __mapper_args__ = {
+      'polymorphic_identity': 'staff',
+    }
+
+    def __init__(self, username, password):
+        self.username = username
+        self.set_password(password)
+        self.user_type = "staff"
