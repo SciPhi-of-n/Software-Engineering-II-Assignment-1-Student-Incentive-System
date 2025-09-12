922e34cf741b96a8ed1ec7209d58c91436d28c6d
SciPhi-of-n <jonathan.bruce@my.uwi.edu>
2025-09-12 13:55:06 -0400

    Added viewAccolades and requestHours to student.py

--- App/models/student.py
+++ App/models/student.py
@@ -1,4 +1,7 @@
 from .user import User
+from .accolade import Accolade
+from .request import Request
+from .record import Record
 from App.database import db
 
 class Student(User):
@@ -7,7 +10,21 @@ class Student(User):
       'polymorphic_identity': 'student',
     }
 
-    def __init__(self, username, password):
-        self.username = username
-        self.set_password(password)
-        self.user_type = "student" 
+  def __init__(self, username, password):
+      self.username = username
+      self.set_password(password)
+      self.user_type = "student"
+
+  def viewAccolades(self):
+    return Accolade.query.filterby(Accolade.studentId = self.id).all()
+
+  def requestHours(self):
+    search = Request.query.filterby(Request.studentId = self.id).all()
+    if search:
+      db.session.rollback()
+      printf("Only one request can be made per student")
+      return none
+    else:
+      request = Request()
+      return createRequest(self.id)
+    
