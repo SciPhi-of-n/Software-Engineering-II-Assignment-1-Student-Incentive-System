aed6d21c4952828a823f82d22fbfd503976c4db2
SciPhi-of-n <jonathan.bruce@my.uwi.edu>
2025-09-13 07:10:11 -0800

    Added relationship to student, removed record id from __repr__ , add "createRecord" method, updated "updateRecord" to find specific records, include session.commit and handle exceptions

--- App/models/record.py
+++ App/models/record.py
@@ -3,8 +3,9 @@ from App.database imprort db
 
 class Record(db.Model):
   recordId = db.Column(db.Integer, primary_key=True)
-  studentId = db.Column(db.Integer, db.ForeignKey('student.id'), nullable=False)
+  studentId = db.Column(db.Integer, db.ForeignKey('student.id'), nullable=False, unique=True)
   hours= db.Column(db.Integer)
+  student = db.relationship('Student', backref=db.backref('record', uselist=False))
 
   def __init__(self, recordId, studentId, hours):
     self.recordId= recordId
@@ -12,8 +13,33 @@ class Record(db.Model):
     self.hours= hours
 
   def __repr__(self):
-    return f'<record {self.recordId} Student {self.student.username} Hours Logged: {self.hours}>'
+    return f'<Student {self.student.username} Hours Logged: {self.hours}>'
 
-  def updateRecord(self, hours):
-    self.hours=hours
-    return
+  def createRecord(self):
+    try:
+      db.session.add(self)
+      db.session.commit()
+      return self
+    except IntegrityError as e:
+      db.session.rollback()
+      printf("There can only be one record per student.")
+      return None
+
+  def updateRecord(studentName, hours):
+    studentFound= Student.query.filter_by(username = studentName).first()
+    if not studentFound:
+      printf("No student of the name {studentName} could be found")
+      return None 
+    record = Record.query.filter_by(studentId = studentFound.id).first()
+    if not record:
+      printf("{studentName} has no record")
+      return None
+    try:
+      record.hours=hours
+      db.session.commit()
+      printf("Record Updated")
+      return record
+    except Expection as e:
+      db.session.rollback()
+      print("Error: ", e)
+      return None
