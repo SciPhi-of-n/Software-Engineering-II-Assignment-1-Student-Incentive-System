231b2a411cda84f097abd35cb4e51cceb7311c11
SciPhi-of-n <jonathan.bruce@my.uwi.edu>
2025-09-14 07:56:16 -0800

    Indented init properly

--- App/models/staff.py
+++ App/models/staff.py
@@ -6,7 +6,7 @@ class Staff(User):
       'polymorphic_identity': 'staff',
     }
 
-    def __init__(self, username, password):
-        self.username = username
-        self.set_password(password)
-        self.user_type = "staff"
+def __init__(self, username, password):
+    self.username = username
+    self.set_password(password)
+    self.user_type = "staff"
