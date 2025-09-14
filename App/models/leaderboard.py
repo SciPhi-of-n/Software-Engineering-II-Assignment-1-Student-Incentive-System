0af5f923e2df38c8ae5b1da5ff39be669962b9f2
SciPhi-of-n <jonathan.bruce@my.uwi.edu>
2025-09-14 08:02:10 -0800

    Changed imprort to import, Changed printf to print, changed it from ,desc to .desc

--- App/models/leaderboard.py
+++ App/models/leaderboard.py
@@ -1,18 +1,18 @@
 from .student import Student
 from .record import Record
-from App.database imprort db
+from App.database import db
 
 class Leaderboard(db.Model):
   students = db.relationship('Student', backref=db.backref('leaderboard', lazy='joined'))
   records = db.relationship('Record', backref=db.backref('leaderboard', lazy='joined'))
 
   def getLeaderboard():
-    return (db.session.query(Student, Record).join(Record, Student.id == Record.studentId).order_by(Record.hours,desc()).all())
+    return (db.session.query(Student, Record).join(Record, Student.id == Record.studentId).order_by(Record.hours.desc()).all())
 
   def displayLeaderboard():
     leaderboard = Leaderboard.getLeaderboard()
-    printf("Leaderboard")
+    print("Leaderboard")
     rank=1
     for student, record in leaderboard:
-      printf("{rank}. {student.name}")
+      print("{rank}. {student.name}")
       rank=rank+1
