b5cc7b406f28c701412779a613fdc8b63d6de5e9
SciPhi-of-n <jonathan.bruce@my.uwi.edu>
2025-09-15 17:21:36 -0800

    Added viewRequests, approveRequest and denyRequest methods

--- App/models/staff.py
+++ App/models/staff.py
@@ -1,4 +1,6 @@
 from .user import User
+from .record import Record
+from .request import Request
 from App.database import db
 
 class Staff(User):
@@ -6,7 +8,20 @@ class Staff(User):
       'polymorphic_identity': 'staff',
     }
 
-def __init__(self, username, password):
+  def __init__(self, username, password):
     self.username = username
     self.set_password(password)
     self.user_type = "staff"
+
+  def logHours(studentId, hours):
+    return Record.updateRecord(studentId, hours)
+  
+  def viewRequests():
+    requests = Request.query.all()
+    print(requests.__repr__())
+
+  def approveRequest(requestId, self):
+    return Request.approveRequest(requestId, self.id)
+  
+  def denyRequest(requestId, self):
+    return Request.denyRequest(requestId, self.id)
