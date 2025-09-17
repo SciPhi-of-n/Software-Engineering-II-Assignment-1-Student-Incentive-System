c1af078ea964fdc0b09e123116ea1f6aff86b796
SciPhi-of-n <jonathan.bruce@my.uwi.edu>
2025-09-17 14:24:24 -0400

    Removed relationships

--- App/models/leaderboard.py
+++ App/models/leaderboard.py
@@ -3,9 +3,6 @@ from .record import Record
 from App.database import db
 
 class Leaderboard(db.Model):
-  students = db.relationship('Student', backref=db.backref('leaderboard', lazy='joined'))
-  records = db.relationship('Record', backref=db.backref('leaderboard', lazy='joined'))
-
   def getLeaderboard():
     return (db.session.query(Student, Record).join(Record, Student.id == Record.studentId).order_by(Record.hours.desc()).all())
 
