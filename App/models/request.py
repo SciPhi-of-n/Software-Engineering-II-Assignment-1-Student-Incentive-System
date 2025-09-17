3a6e2fe99af7cbd38cdad9aea9e890fcf1a2fe92
SciPhi-of-n <jonathan.bruce@my.uwi.edu>
2025-09-17 12:59:29 -0400

    Removed db.relationship and everything staff related

--- App/models/request.py
+++ App/models/request.py
@@ -1,39 +1,30 @@
 from .student import Student
-from .staff import Staff
 from App.database import db
 
 class Request(db.Model):
-   __tablename__ = 'requests'
    requestId= recordId = db.Column(db.Integer, primary_key=True)
    studentId = db.Column(db.Integer, db.ForeignKey('student.id'), nullable=False)
-   staffId = db.Column(db.Integer, db.ForeignKey('staff.id'), nullable=False)
    status = db.Column(db.String(30))#default=pending, approved, denied
-   student= db.relationship('Student', backref=db.backref('requests', uselist=False))
-   staff= db.relationship('Student', backref=db.backref('handled_request', lazy='joined'))
 
    def __init__(self, studentId):
      self.studentId=studentId
-     self.staffId=None
      self.status="pending"
 
    def __repr__(self):
-    return f'<Request for Hours no. {self.requestId} Student {self.student.username} Staff: {self.staff.username} Status: {self.status}>'
+    return f'<Request no. {self.requestId} Student {self.student.username} Status: {self.status}>'
 
-   def createRequest(studentId):
+   def createRequest():
      newRequest= Request()
-     newRequest.studentId = studentId
      db.session.add(newRequest)
      db.session.commit()
      return newRequest
 
-   def approveRequest(selectedRequest, staffId):
+   def approveRequest(selectedRequest):
      try:
         request = Request.query.filter_by(requestId = selectedRequest).first()
         if request is None:
            print("Request not found")
            return None
-           
-        request.staffId= staffId
         request.status="approved"
         db.session.commit()
         return request
@@ -42,14 +33,12 @@ class Request(db.Model):
         print("Error: ", e)
         return None
 
-   def denyRequest(selectedRequest, staffId):
+   def denyRequest(selectedRequest):
      try:
         request = Request.query.filter_by(requestId = selectedRequest).first()
         if request is None:
            print("Request not found")
            return None
-           
-        request.staffId= staffId
         request.status="denied"
         db.session.commit()
         return request
